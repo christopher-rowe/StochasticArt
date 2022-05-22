99bfb103bae1ba0c9de4f70fea49e8bb7f5470ef
Chris Rowe <chris.lloyd.rowe@gmail.com>
2022-05-22 15:31:08 -0700

    updates

--- R/panels.R
+++ R/panels.R
@@ -2,13 +2,15 @@
 #'
 #' @param num_panels the number of panels in the artwork; if unspecified, a value
 #' will be chosen randomly
+#' @param square a boolean value, set to TRUE for the artwork to be square
+#' @param single_color a boolean value, set to TRUE to use a single color
 #' @param seed a numeric value, the seed to initialize psuedo-randomness
 #'
 #' @export
 #'
 #' @return a random work of art as a ggplot object
 #'
-generate_panels = function(num_panels, seed){
+generate_panels = function(num_panels, square = TRUE, single_color = TRUE, seed){
 
   # set & print seed
   if(missing(seed)){seed = sample(1:1e8, 1)}
@@ -25,13 +27,17 @@ generate_panels = function(num_panels, seed){
 
   # generate bottom & top panel data
   bottom_panel_data = dplyr::tibble(
-    xmin = xmin, xmax = xmax,
-    ymin = rep(0,num_panels), ymax = runif(num_panels)
+    xmin = xmin,
+    xmax = xmax,
+    ymin = {if(square){rep(0,num_panels)} else {runif(num_panels, -0.25, 0)}},
+    ymax = runif(num_panels)
   )
 
   top_panel_data = dplyr::tibble(
-    xmin = xmin, xmax = xmax,
-    ymin = bottom_panel_data$ymax, ymax = rep(1,num_panels)
+    xmin = xmin,
+    xmax = xmax,
+    ymin = bottom_panel_data$ymax,
+    ymax = {if(square){rep(1,num_panels)} else {runif(num_panels, 1, 1.25)}},
   )
 
   # randomly pick a color
@@ -39,6 +45,15 @@ generate_panels = function(num_panels, seed){
     red = sample(0:255, 1), blue = sample(0:255, 1), green = sample(0:255, 1),
     maxColorValue = 255
   )
+
+  # assign bottom/top colors to same or different colors as specified
+  if(single_color){
+    bottom_color = color
+    top_color = color
+  } else {
+    bottom_color = color
+    top_color = colortools::adjacent(color, plot=F)[sample(2:3,1)]
+  }
 
   # flip the alpha coin to determine the direction of the alpha gradient
   alpha_coin = rbinom(1, 1, 0.5) == 1
@@ -48,15 +63,16 @@ generate_panels = function(num_panels, seed){
     ggplot2::geom_rect(
       data = bottom_panel_data,
       ggplot2::aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax, alpha = if(alpha_coin){xmin} else{1-xmin}),
-      fill = color, col = 'white') +
+      fill = bottom_color, col = 'white') +
     ggplot2::geom_rect(
       data = top_panel_data,
       ggplot2::aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax, alpha = if(alpha_coin){1-xmin} else{xmin}),
-      fill = color, col = 'white') +
-    ggplot2::xlim(0, 1) +
-    ggplot2::ylim(0, 1) +
+      fill = top_color, col = 'white') +
+    ggplot2::xlim(-0.25, 1.25) +
+    ggplot2::ylim(-0.25, 1.25) +
     ggplot2::theme_classic() +
-    ggplot2::theme(axis.line = ggplot2::element_blank(),
+    ggplot2::theme(aspect.ratio = 1,
+                   axis.line = ggplot2::element_blank(),
                    axis.text.x = ggplot2::element_blank(),
                    axis.text.y = ggplot2::element_blank(),
                    axis.ticks = ggplot2::element_blank(),
